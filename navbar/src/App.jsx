254b1b9890c4130b2a2a4bbb88a5b6e18bfe2970
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2024-03-10 23:15:40 +0100

    .

---
+++ navbar/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar"
+
+export default function App() {
+
+  return(
+    <>
+      <nav>
+        <Navbar />
+      </nav>
+      
+    </>
+  )
+}
---
+++ navbar/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import fitPulseLogo from '../assets/fitpulse.svg'
+
+export default function Navbar() {
+
+    return(
+        <div>
+            <div id="image">
+                <a href="/"><img src={fitPulseLogo} alt="" className=''/></a>
+            </div>
+            
+        </div>
+    )
+}
